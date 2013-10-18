aa943f4d451e2fd49f5108f16085fa9d92bb9630
Will Beasley <wibeasley@hotmail.com>
2013-10-17 20:54:00 -0500

    Reformat & alphabetize

--- CodeUtilities/PackageInstallation.R
+++ CodeUtilities/PackageInstallation.R
@@ -4,37 +4,35 @@
 rm(list=ls(all=TRUE)) #Clear the memory for any variables set from any previous runs.
 
 packagesToInstall <- c(
-     "RCurl" #For interacting with the REDCap API
-   , "RODBC" #For connecting to ODBC databases
-   , "knitr" #For reporting
-   , "ggplot2" #Graphing
-   , "ggthemes" #Extra themes, scales and geoms for ggplot
-   , "plyr" #Important for most of our data manipulation
-   , "reshape2" #data manipulation not covered in plyr (possibly not necessary anymore)
-   , "devtools" #CUsed in the C1 survival for sourcing gists
-   , "stringr" #Consistent/convienent function signatures for manipulating text
-   , "lubridate" #Consistent/convienent function signatures for manipulating dates
-   , "evaluate" #A package that Hadley et al use a lot in their packages.  It helps when things are passed by reference.
-   , "colorspace" #Explicit control over the HCL color scheme
-   , "rjags" #Interact with the Bayesian modeling software, JAGS
-   , "R2OpenBUGS" #Interact with the Bayesian modeling software, OpenBugs
-   , "RColorBrewer" #Explicit control over the Color Brewer colors.  See http://colorbrewer2.org/
-   , "Rcpp" #Optimizing some intensive functions
-   , "wordcloud" #Creating the graphics of word frequencies
-   , "tm" #Text Mining
-   , "digest" #Creates SHA hashes for the recruiting database
-   , "random" #Creates random numbers for salts
-   , "xtable" #Creates formatted tables. Especially nice for LaTeX
-   , "modeest" #Estimating the mode of unimodal distributions
-   , "classInt" #A package for choosing univariate class intervals for mapping or other graphics purposes
-   , "foreign" #Reads data in other formats
-     , "lme4" #Multilevel models
-     , "car" #accompanies Fox, J. and Weisberg, S., An R Companion to Applied Regression,
-     , "R2WinBUGS" #Interact with the Bayesian modeling software, WinBugs
-     , "sp" #Spatial analysis
-     , "spdep" #Spatial dependence; builds on 'sp'
+  "car" #Accompanies Fox, J. and Weisberg, S., An R Companion to Applied Regression,
+  , "classInt" #Choosing univariate class intervals for mapping or other graphics purposes
+  , "colorspace" #Explicit control over the HCL color scheme
+  , "devtools" #Used in the C1 survival for sourcing gists
+  , "digest" #Creates SHA hashes for the recruiting database
+  , "evaluate" #A package that Hadley et al use a lot in their packages.  It helps when things are passed by reference.
+  , "foreign" #Reads data in other formats
+  , "ggplot2" #Graphing
+  , "ggthemes" #Extra themes, scales and geoms for ggplot
+  , "knitr" #For reporting
+  , "lme4" #Multilevel models
+  , "lubridate" #Consistent/convienent function signatures for manipulating dates
+  , "modeest" #Estimating the mode of unimodal distributions
+  , "plyr" #Important for most of our data manipulation
+  , "R2OpenBUGS" #Interact with the Bayesian modeling software, OpenBugs
+  , "R2WinBUGS" #Interact with the Bayesian modeling software, WinBugs
+  , "random" #Creates random numbers for salts
+  , "RColorBrewer" #Explicit control over the Color Brewer colors.  See http://colorbrewer2.org/
+  , "Rcpp" #Optimizing some intensive functions
+  , "RCurl" #Interact with the REDCap API
+  , "reshape2" #Data manipulation not covered in plyr
+  , "rjags" #Interact with the Bayesian modeling software, JAGS
+  , "RODBC" #For connecting to ODBC databases
+  , "sp" #Spatial analysis
+  , "spdep" #Spatial dependence; builds on 'sp', "xtable" #Creates formatted tables. Especially nice for LaTeX
+  , "stringr" #Consistent/convienent function signatures for manipulating text
+  , "tm" #Text Mining
+  , "wordcloud" #Creating the graphics of word frequencies  
 ) 
-#"devtools",
 
 for( packageName in packagesToInstall ) {
   available <- require(packageName, character.only=TRUE) #Loads the packages, and indicates if it's available
