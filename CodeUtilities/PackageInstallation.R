3efe639064572a2c6286ebbc77cb4cee8579e1e9
Will Beasley <wibeasley@hotmail.com>
2013-11-12 09:34:34 -0600

    tesit

--- CodeUtilities/PackageInstallation.R
+++ CodeUtilities/PackageInstallation.R
@@ -32,7 +32,6 @@ packagesToInstall <- c(
   , "sp" #Spatial analysis
   , "spdep" #Spatial dependence; builds on 'sp', "xtable" #Creates formatted tables. Especially nice for LaTeX
   , "stringr" #Consistent/convienent function signatures for manipulating text
-  , "testthat" #Heavy testing framework
   , "tm" #Text Mining
   , "wordcloud" #Creating the graphics of word frequencies  
   , "xtable" #Formats tables, especially for LaTeX output.
