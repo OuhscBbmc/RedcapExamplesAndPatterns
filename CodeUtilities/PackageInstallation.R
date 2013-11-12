5cab3eaf1180b1fcefb93cbb078ffb08adddcb44
Will Beasley <wibeasley@hotmail.com>
2013-11-12 09:31:38 -0600

    testit again

--- CodeUtilities/PackageInstallation.R
+++ CodeUtilities/PackageInstallation.R
@@ -32,10 +32,7 @@ packagesToInstall <- c(
   , "sp" #Spatial analysis
   , "spdep" #Spatial dependence; builds on 'sp', "xtable" #Creates formatted tables. Especially nice for LaTeX
   , "stringr" #Consistent/convienent function signatures for manipulating text
-  , "testit" #Light testing framework, includes useful `assert()` function.
   , "testthat" #Heavy testing framework
-  , "testit" #has the useful `assert()` function
-  , "testit" #has the useful `assert()` function
   , "tm" #Text Mining
   , "wordcloud" #Creating the graphics of word frequencies  
   , "xtable" #Formats tables, especially for LaTeX output.
