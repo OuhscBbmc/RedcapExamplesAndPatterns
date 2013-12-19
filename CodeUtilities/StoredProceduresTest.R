eacae2c3b8bf3d1a0fc55781fb52fcb02becfc1b
Will Beasley <wibeasley@hotmail.com>
2013-12-19 15:48:03 -0600

    qualifying functions with package names

--- CodeUtilities/StoredProceduresTest.R
+++ CodeUtilities/StoredProceduresTest.R
@@ -1,5 +1,7 @@
 require(RODBC)
-channel <- RODBC::odbcConnect("MiechvEvaluation") #getSqlTypeInfo("Microsoft SQL Server") #odbcGetInfo(channel)
+channel <- RODBC::odbcConnect("MiechvEvaluation") 
+RODBC::getSqlTypeInfo("Microsoft SQL Server") 
+RODBC::odbcGetInfo(channel)
 
 redcapUrl <- RODBC::sqlQuery(channel, "EXEC Security.prcUri @UriName = 'Redcap1'", stringsAsFactors=FALSE)[1, 'Value']
 token <- RODBC::sqlQuery(channel, "EXEC [Security].[prcRedcapToken] @Username = 'ReportPhiFree', @RedcapProjectName = 'Gpav2'", stringsAsFactors=FALSE)[1, 'Token']
