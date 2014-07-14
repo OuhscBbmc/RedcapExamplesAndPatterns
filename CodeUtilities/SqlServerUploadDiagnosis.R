d33ce71801de67a9bb692b0613262093c178d36a
Will Beasley <wibeasley@hotmail.com>
2014-07-14 11:48:01 -0500

    renaming sql diagnosis dataset

--- CodeUtilities/SqlServerUploadDiagnosis.R
+++ CodeUtilities/SqlServerUploadDiagnosis.R
@@ -12,41 +12,44 @@ sapply(ds, function(x) max(nchar(x)))
 ## Specify columns & their order to upload
 columnsToWrite <- c("ID", "NameFull", "StartDate", "StopDate", "ElapsedMonths", "CensoredLeft", "CensoredRight",
                     "Position", "Comments",  "PersonID_1", "PersonID_2", "ReviewDate", "UploadToSqlDate")
-ds <- ds[, columnsToWrite]
+dsSlim <- ds[, columnsToWrite]
 
 startTime <- base::Sys.time()
-dbTable <- "Osdh.tblNurseStartStopDate"
+dbTable <- "Osdh.tblC1ClientActivity"
 channel <- RODBC::odbcConnect("MiechvEvaluation") 
 RODBC::getSqlTypeInfo("Microsoft SQL Server") 
 RODBC::odbcGetInfo(channel)
 columnInfo <- RODBC::sqlColumns(channel, dbTable)
 varTypes <- as.character(columnInfo$TYPE_NAME) 
 names(varTypes) <- as.character(columnInfo$COLUMN_NAME)  #varTypes
-RODBC::sqlSave(channel, ds, dbTable, append=TRUE, rownames=FALSE, fast=TRUE, varTypes=varTypes) 
+RODBC::sqlSave(channel, dsSlim, dbTable, append=TRUE, rownames=FALSE, fast=TRUE, varTypes=varTypes) 
 RODBC::odbcClose(channel)
 (elapsedDuration <-  Sys.time() - startTime) # 0.519052 secs 2013-10-27
 
 #Visually inspect the dataset you're trying to upload
-head(ds, 15)
-summary(ds)
+head(dsSlim, 15)
+summary(dsSlim)
 
 #See if the names match exactly
-data.frame(DBColumnName=as.character(columnInfo$COLUMN_NAME),
-           DFVariableName=colnames(ds),
-           Violation=(colnames(ds)!=as.character(columnInfo$COLUMN_NAME)))
+data.frame(DBColumnName = as.character(columnInfo$COLUMN_NAME),
+           DFVariableName = colnames(dsSlim),
+           Violation = (colnames(dsSlim)!=as.character(columnInfo$COLUMN_NAME))
+)
 
 #See if the there's a violoation of nulls
-data.frame(ColumnName=colnames(ds), 
-           DBAllowsNulls=as.logical(columnInfo$NULLABLE), 
-           DFHasNulls=sapply(ds, function(x) { any(is.na(x)) }),
-           Violation=(!as.logical(columnInfo$NULLABLE) & sapply(ds, function(x) { any(is.na(x)) })))
+data.frame(ColumnName = colnames(dsSlim), 
+           DBAllowsNulls = as.logical(columnInfo$NULLABLE), 
+           DFHasNulls = sapply(dsSlim, function(x) { any(is.na(x)) }),
+           Violation = (!as.logical(columnInfo$NULLABLE) & sapply(dsSlim, function(x) { any(is.na(x)) }))
+)
 
 #See if types are consistent
-data.frame(DBType=varTypes, DFType=sapply(ds, class))
+data.frame(DBType=varTypes, DFType=sapply(dsSlim, class))
 
 #See if the there's a violoation of characters being too long. This only works for strings, not dates or numbers
-data.frame(ColumnName=colnames(ds), 
-           DBType=varTypes,
-           DBMaxSize=columnInfo$COLUMN_SIZE, 
-           DFMaxSize=sapply(ds, function(x) { max(nchar(x)) }),
-           PossibleViolation=(!as.logical(columnInfo$COLUMN_SIZE) & sapply(ds, function(x) { max(nchar(x)) })))
+data.frame(ColumnName = colnames(dsSlim), 
+           DBType = varTypes,
+           DBMaxSize = columnInfo$COLUMN_SIZE, 
+           DFMaxSize = sapply(dsSlim, function(x) { max(nchar(x)) }),
+           PossibleViolation = (!as.logical(columnInfo$COLUMN_SIZE) & sapply(dsSlim, function(x) { max(nchar(x)) }))
+)
