7590b9c891f973276723280cb68ac5b604bfd70d
Will Beasley <wibeasley@hotmail.com>
2016-05-10 16:47:36 -0500

    tidy CSV reading

--- utility/read-csv-utilities.R
+++ utility/read-csv-utilities.R
@@ -1,43 +1,38 @@
 #http://r.789695.n4.nabble.com/some-helpful-tips-on-using-RODBC-td2324503.html
-rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
-library(plyr, quietly=TRUE)
+rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
+
+# ---- load-sources ------------------------------------------------------------
+
+# ---- load-packages -----------------------------------------------------------
+library("magrittr")
 library(RODBC, quietly=TRUE)
-library(lubridate, quietly=TRUE)
-library(stringr, quietly=TRUE)
+requireNamespace("dplyr")
+requireNamespace("readr")
+requireNamespace("testit")
 
-#############################
-### Declare global functions and constants
-#############################
+# ---- declare-globals ---------------------------------------------------------
 
-
-#############################
-### Retrieve token and REDCap URL
-#############################
-dsnName <- "MiechvEvaluation"
-channel <- RODBC::odbcConnect(dsnName) #getSqlTypeInfo("Microsoft SQL Server") #odbcGetInfo(channel)
+# ---- load-data ---------------------------------------------------------------
+channel <- RODBC::odbcConnect("MiechvEvaluation") #getSqlTypeInfo("Microsoft SQL Server") #odbcGetInfo(channel)
 uriExtract <- RODBC::sqlQuery(channel, "EXEC Security.prcUri @UriName = 'C1Clinic'", stringsAsFactors=FALSE)[1, 'Value']
-RODBC::odbcClose(channel)
+RODBC::odbcClose(channel); rm(channel)
 
-rm(channel, dsnName)
-#############################
-### Read the CSV and rename some columns
-#############################
-ds <- read.csv(uriExtract, stringsAsFactors=FALSE, na.strings="", nrows=-100)
-classesAutomatic <- sapply(ds, class)
-
-definition <- ""
-for( i in seq_along(classesAutomatic) ) {
-  definition <- paste0(definition, ", \"", names(classesAutomatic)[i], "\"=\"", (classesAutomatic)[i], "\"\n")
-}
-cat(definition) #Copy this output from the console and paste it below in `classes`.  And probably in the `TransferZZZExtractToDB.R` file too.
+# ds <- read.csv(uriExtract, stringsAsFactors=FALSE, na.strings="", nrows=-100)
+# classesAutomatic <- sapply(ds, class)
+# 
+# definition <- ""
+# for( i in seq_along(classesAutomatic) ) {
+#   definition <- paste0(definition, ", \"", names(classesAutomatic)[i], "\"=\"", (classesAutomatic)[i], "\"\n")
+# }
+# cat(definition) #Copy this output from the console and paste it below in `classes`.  And probably in the `TransferZZZExtractToDB.R` file too.
 
 classes <- c(
-  "ClinicID"="integer"
-  , "SiteID"="integer"
-  , "Clinic_No"="character" #Change from 'integer' to character, so leading zeros aren't dropped.
-  , "Clinic_Name"="character"
-  , "CountyID"="integer"
-  , "County"="character"
+  "ClinicID"        = "integer"
+  , "SiteID"        = "integer"
+  , "Clinic_No"     = "character" #Change from 'integer' to character, so leading zeros aren't dropped.
+  , "Clinic_Name"   = "character"
+  , "CountyID"      = "integer"
+  , "County"        = "character"
 )
 # paste0("Visit", 1:93, =)
 
@@ -46,3 +41,17 @@ system.time(
 )
 
 summary(ds)
+
+# ---- tweak-data --------------------------------------------------------------
+
+
+
+# ---- verify-values -----------------------------------------------------------
+testit::assert("All IDs should be nonmissing and positive.", all(!is.na(ds$ClinicID) & (ds$ClinicID>0)))
+
+# ---- specify-columns-to-upload -----------------------------------------------
+
+
+# ---- upload-to-db ------------------------------------------------------------
+
+# ---- save-to-disk ------------------------------------------------------------
