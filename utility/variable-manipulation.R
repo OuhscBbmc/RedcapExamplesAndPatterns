3c5068cb0194e610cdd69ec232311975038f4b55
Will Beasley <wibeasley@hotmail.com>
2016-05-10 16:40:18 -0500

    Redirect to replace-with-nas
    https://github.com/OuhscBbmc/OuhscMunge/blob/master/R/replace-with-nas.R

--- utility/variable-manipulation.R
+++ utility/variable-manipulation.R
@@ -1,11 +1,4 @@
-#This seemed like the best place to put this code for now.  It probably deserves to be in some package.
+requireNamespace("OuhscMunge")  # devtools::install_github("OuhscBbmc/OuhscMunge")
+# https://github.com/OuhscBbmc/OuhscMunge/blob/master/R/replace-with-nas.R
 
-ReplaceNAsWithFactorLevel <- function( scores, newNALabel="Unknown", addUnknownLevel=FALSE) {
-  if( addUnknownLevel ) {
-    levels(scores) <- c(levels(scores), newNALabel)  #Add a new level called 'Unknown', or whatever the caller specified
-  }
-  
-  scores[is.na(scores)] <- newNALabel #eg, "Unknown"
-  if( any(is.na(scores)) ) stop("The reassigned factor variable should not have any NA values.")
-  return( scores )
-}
+# OuhscMunge::replace_with_nas()
