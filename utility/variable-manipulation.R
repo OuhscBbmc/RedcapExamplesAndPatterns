aece0d3e6d44aadb4ae08dbf485c431493093d33
Will Beasley <wibeasley@hotmail.com>
2020-01-22 13:15:47 -0600

    Delete variable-manipulation.R

--- utility/variable-manipulation.R
+++
@@ -1,4 +0,0 @@
-requireNamespace("OuhscMunge")  # devtools::install_github("OuhscBbmc/OuhscMunge")
-# https://github.com/OuhscBbmc/OuhscMunge/blob/master/R/replace-with-nas.R
-
-# OuhscMunge::replace_with_nas()
